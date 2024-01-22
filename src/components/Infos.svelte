91a72a411d38256d352199cabd395039231602c6
Yeeloman <yami.onlyme@gmail.com>
2024-01-22 18:37:32 +0100

    add files

---
+++ src/components/Infos.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" >
+  let isvisible = false;
+  export let title= "";
+  export let content= "";
+  const plus = '/images/icon-plus.svg';
+  const minus = '/images/icon-minus.svg';
+
+  function togglevisibility() {
+    isvisible = !isvisible;
+  }
+</script>
+<div class="py-5">
+  <div class="flex items-center mx-7 justify-between">
+    <p class="font-semibold 
+      text-md 
+      text-black 
+      hover:text-violet-500 hover:cursor-pointer" 
+      on:click={togglevisibility} >{title}</p>
+    <img
+      src={isvisible ? minus : plus}
+      alt="plus"
+      on:click={togglevisibility}
+      class="w-7 h-7 hover:cursor-pointer"
+    >
+  </div>
+  {#if isvisible}
+    <div class="pt-5">
+      <p class="text-gray-500 font-normal mx-7">{content}</p>
+    </div>
+  {/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Infos from '$components/Infos.svelte';
+  let background = "/images/background-pattern-desktop.svg";
+  let star = "/images/icon-star.svg";
+  let data = {
+    faq1: {
+      question: "How to do something?",
+      answer: "Here is the answer to your question.",
+    },
+    faq2: {
+      question: "What is something?",
+      answer: "Here is the explanation of that something.",
+    },
+    faq3: {
+      question: "Why is something important?",
+      answer: "Here is why it matters.",
+    },
+    faq4: {
+      question: "When does something happen?",
+      answer: "Here is the timeline of events.",
+    },
+  };</script>
+<div class="relative">
+  <img src={background} alt="background">
+   <div class="flex items-center justify-center top-2/3">
+    <div class="bg-white rounded-lg h-auto w-2/5 absolute z-1 top-2/3">
+      <div class="flex py-5 items-center space-x-4">
+        <img src={star} alt="star" class="ml-7 h-9 w-9">
+        <h1 class="text-5xl font-bold">FAQs</h1>
+      </div>
+      {#each Object.values(data) as {question, answer}}
+        <Infos title={question} content={answer}/>
+      {/each}
+    </div>
+  </div>
+</div>
